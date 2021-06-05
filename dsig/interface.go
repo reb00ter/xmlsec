4377747356220b6fa7a50ecf44e03e099297212c
Maxim Barabanov <maxim.barabanov@megafon.ru>
2021-06-05 09:01:03 +0300

    add Gost transforms

--- dsig/interface.go
+++ dsig/interface.go
@@ -9,11 +9,13 @@ import (
 type TransformID clib.TransformID
 
 var (
-	ExclC14N  = TransformID(clib.ExclC14N)
-	InclC14N  = TransformID(clib.InclC14N)
-	Enveloped = TransformID(clib.Enveloped)
-	Sha1      = TransformID(clib.Sha1)
-	RsaSha1   = TransformID(clib.RsaSha1)
+	ExclC14N     = TransformID(clib.ExclC14N)
+	InclC14N     = TransformID(clib.InclC14N)
+	Enveloped    = TransformID(clib.Enveloped)
+	Sha1         = TransformID(clib.Sha1)
+	RsaSha1      = TransformID(clib.RsaSha1)
+	Gost2012_256 = TransformID(clib.Gost2012_256)
+	Gost2012_512 = TransformID(clib.Gost2012_512)
 )
 
 type Ctx struct {
